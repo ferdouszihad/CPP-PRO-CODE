7910f698e07101b7fc0f7ca6a625d603439263c5
ferdouszihad <f.zihad333@gmail.com>
2023-10-02 18:48:02 +0600

    Day01 completed

---
+++ prininng.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float x;
+    cin >> x;
+    cout << fixed << setprecision(2) << "My Money - " << x << " TK" << endl;
+
+    return 0;
+}
